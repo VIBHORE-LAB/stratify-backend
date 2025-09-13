d478a9c633efab35b22fa9ce6dc561a329cf18ff
Vibhore Mathur <onlyvibhore@gmail.com>
2025-09-13 15:31:16 +0530

    Add build.sh to compile C++ backtester

--- build.sh
+++ build.sh
@@ -1,9 +1,16 @@
+#!/usr/bin/env bash
 set -e
 
-cp cppEngine
+# Navigate to cppEngine folder
+cd cppEngine
+
+# Create build folder if it doesn't exist
 mkdir -p build
 cd build
+
+# Run cmake and build
 cmake ..
 make -j$(nproc)
 
+# Make sure the binary is executable
 chmod +x backtester
